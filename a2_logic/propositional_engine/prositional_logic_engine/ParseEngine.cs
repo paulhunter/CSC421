5c1a43982b69d7b54f91ce1fd716663af19441e8
paulhunter <p.hunter.eng@gmail.com>
2015-02-10 12:44:26 -0800

    Start of A2

---
+++ a2_logic/propositional_engine/prositional_logic_engine/ParseEngine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prositional_logic_engine
+{
+    /// <summary>
+    /// This parse engine of in-fix notation for propositional logic produces
+    /// a resulting parse tree or appropriate exception. 
+    /// 
+    /// The Syntax for Propositional logic
+    /// 
+    /// </summary>
+    public class ParseEngine
+    {
+
+        public bool TryParse(string Input, out string RPN, out Exception Error)
+        {
+
+
+            RPN = null;
+            Error = null;
+            return false;
+        }
+
+
+
+
+
+        
+    }
+}
---
+++ a2_logic/propositional_engine/prositional_logic_engine/ParseToken.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prositional_logic_engine
+{
+    public class ParseToken
+    {
+        Dictionary<Operation, string> Keywords = new Dictionary<Operation, string>()
+            {
+                {Operation.NOT, "NOT" },
+                {Operation.AND, "AND" },
+                {Operation.OR, "OR" },
+                {Operation.IF, "->" }, 
+                {Operation.IFF, "<->"},
+                {Operation.LEFT_PARATHESIS, "("},
+                {Operation.RIGHT_PARATHESIS, ")"}
+            };
+
+        enum Type
+        {
+            OPERATION,
+            SYMBOL
+        }
+
+        /// <summary>
+        /// Operations are enumerated by precedence
+        /// </summary>
+        enum Operation : byte
+        {
+            NOT = 5,
+            AND = 4,
+            OR = 3,
+            IF = 2,
+            IFF = 1,
+            LEFT_PARATHESIS = 0,
+            RIGHT_PARATHESIS = 0
+        }
+
+        public ParseToken(string SymbolName)
+        {
+            this.type = Type.SYMBOL;
+            this.symbol = SymbolName;
+        }
+
+        public ParseToken(Operation Op)
+        {
+            this.type = Type.OPERATION;
+            this.symbol = Keywords[op];
+        }
+
+        public Type type;
+        public Operation op;
+        public string symbol;
+
+    }
+}
